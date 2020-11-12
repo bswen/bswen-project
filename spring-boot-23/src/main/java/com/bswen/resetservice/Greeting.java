00054519811a39b59381b282f82585d604ba580a
bswen <bswenapp@gmail.com>
2020-11-12 17:58:21 +0800

    add a springboot23 module use springboot2.3 and gradle 5.6 to build the project

---
+++ spring-boot-23/src/main/java/com/bswen/resetservice/Greeting.java
@@ -0,0 +1,19 @@
+package com.bswen.resetservice;
+
+public class Greeting {
+    private long id;
+    private String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
